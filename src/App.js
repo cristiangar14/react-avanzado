eb96f640e429337e56cf4111a1f96047199ae451
Cristian Garzon Sanchez <cristiangar14@hotmail.com>
2021-07-28 22:18:56 -0500

    estilos basicos

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ListOfCategories } from './components/listOfCategories'
+import { GlobalStyle } from './GlobalStyles'
+
+export const App = () => (
+  <div>
+    <GlobalStyle />
+    <ListOfCategories />
+  </div>
+)
